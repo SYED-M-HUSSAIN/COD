75c4cb8d046fe4047800481ea7405d4d525663d1
Muhammad Hussain <88007126+SYED-M-HUSSAIN@users.noreply.github.com>
2023-09-11 23:11:28 +0500

    Update Mnas.py

--- Mnas.py
+++ Mnas.py
@@ -1,71 +1,131 @@
-import tensorflow as tf
-from tensorflow.keras.layers import Input, GlobalAveragePooling2D, Dense
-from tensorflow.keras.models import Model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import MobileNetV2
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
 
-# Define your data directories
-train_data_dir = 'data/train'
-validation_data_dir = 'data/validation'
+# Define MnasNet architecture
+class SepConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super(SepConvBlock, self).__init__()
+        self.depthwise_conv = nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding, groups=in_channels)
+        self.pointwise_conv = nn.Conv2d(in_channels, out_channels, 1)
+        self.relu = nn.ReLU(inplace=True)
+    
+    def forward(self, x):
+        x = self.depthwise_conv(x)
+        x = self.pointwise_conv(x)
+        x = self.relu(x)
+        return x
 
-# Define hyperparameters
-input_shape = (224, 224, 3)
-num_classes = 2
-batch_size = 32
-epochs = 10
+class InvertedResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, expansion_factor, stride):
+        super(InvertedResidualBlock, self).__init__()
+        hidden_dim = in_channels * expansion_factor
+        self.use_residual = stride == 1 and in_channels == out_channels
 
-# Create the MobileNetV2 model
-def create_mobilenetv2_model(input_shape, num_classes):
-    input_tensor = Input(shape=input_shape)
-    base_model = MobileNetV2(input_shape=input_shape, include_top=False, weights='imagenet')
-    for layer in base_model.layers:
-        layer.trainable = False
-    x = GlobalAveragePooling2D()(base_model.output)
-    x = Dense(128, activation='relu')(x)
-    output = Dense(num_classes, activation='softmax')(x)
-    model = Model(inputs=input_tensor, outputs=output)
-    return model
+        layers = []
+        if expansion_factor != 1:
+            layers.append(nn.Conv2d(in_channels, hidden_dim, 1))
+            layers.append(nn.ReLU(inplace=True))
+        
+        layers.extend([
+            SepConvBlock(hidden_dim, hidden_dim, stride=stride),
+            nn.Conv2d(hidden_dim, out_channels, 1)
+        ])
 
-# Create the model
-model = create_mobilenetv2_model(input_shape, num_classes)
+        self.conv_block = nn.Sequential(*layers)
 
-# Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    def forward(self, x):
+        if self.use_residual:
+            return x + self.conv_block(x)
+        else:
+            return self.conv_block(x)
 
-# Data augmentation for the training data
-train_datagen = ImageDataGenerator(
-    rescale=1.0/255.0,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True
-)
+class MnasNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(MnasNet, self).__init__()
+        # Initial Convolution Layer
+        self.initial_conv = nn.Sequential(
+            nn.Conv2d(1, 32, 3, 2, 1),
+            nn.ReLU(inplace=True)
+        )
+        
+        # Inverted Residual Blocks
+        self.blocks = nn.Sequential(
+            InvertedResidualBlock(32, 16, 1, 1),
+            InvertedResidualBlock(16, 24, 6, 2),
+            InvertedResidualBlock(24, 24, 6, 1),
+            InvertedResidualBlock(24, 32, 6, 2),
+            InvertedResidualBlock(32, 32, 6, 1),
+            InvertedResidualBlock(32, 64, 6, 2),
+            InvertedResidualBlock(64, 64, 6, 1),
+        )
+        
+        # Final Classification Layer
+        self.classifier = nn.Sequential(
+            nn.Conv2d(64, 1280, 1),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(1)
+        )
 
-# No data augmentation for validation data
-validation_datagen = ImageDataGenerator(rescale=1.0/255.0)
+        self.fc = nn.Linear(1280, num_classes)
 
-# Create data generators
-train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size=input_shape[:2],
-    batch_size=batch_size,
-    class_mode='categorical'
-)
+    def forward(self, x):
+        x = self.initial_conv(x)
+        x = self.blocks(x)
+        x = self.classifier(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
 
-validation_generator = validation_datagen.flow_from_directory(
-    validation_data_dir,
-    target_size=input_shape[:2],
-    batch_size=batch_size,
-    class_mode='categorical'
-)
+# Hyperparameters
+num_epochs = 10
+batch_size = 64
+learning_rate = 0.001
 
-# Train the model
-model.fit(
-    train_generator,
-    steps_per_epoch=train_generator.samples // batch_size,
-    validation_data=validation_generator,
-    validation_steps=validation_generator.samples // batch_size,
-    epochs=epochs
-)
+# Data preprocessing
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
 
-# Save the trained model
-model.save('mobilenetv2_binary_classification.h5')
+trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+
+# Model, loss, and optimizer
+mnasnet = MnasNet()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(mnasnet.parameters(), lr=learning_rate)
+
+# Training loop
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+
+        optimizer.zero_grad()
+
+        outputs = mnasnet(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+    print(f'Epoch {epoch + 1}, Loss: {running_loss / len(trainloader)}')
+
+# Testing loop
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        inputs, labels = data
+        outputs = mnasnet(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy on test set: {(100 * correct / total):.2f}%')
